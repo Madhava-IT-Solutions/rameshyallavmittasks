69a5ac5e33e5e5a67da4ecad23445a45c4be684b
rameshyallamadhavaitsolutions <rameshyallamadhavaitsolutions@gmail.com>
2025-01-04 16:09:31 +0530

    madhava

--- tenders2/server2.js
+++ tenders2/server2.js
@@ -14,6 +14,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
 
 
 const uploadsDir = path.join(__dirname, 'uploads');
